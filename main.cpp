7daa33bb14dea2eabb83565a414968fbc14b2427
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2025-02-15 14:31:33 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
-//========================================================================
 int main() {
 	ofGLFWWindowSettings settings;
-	settings.setGLVersion(3, 2); //we define the OpenGL version we want to use
+	settings.setGLVersion(3, 2);
 	settings.setSize(1024, 768);
 	ofCreateWindow(settings);
-	ofSetFullscreen(	true);
+	ofSetFullscreen(false);
 	ofRunApp(new ofApp());
 }
